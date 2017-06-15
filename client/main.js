ccb485bfbe4f630275162ed399c74e1e8da6bfbf
Kyle Ducharme <kyleducharme@Kyles-MBP.lan>
2017-06-14 21:30:00 -0500

    Updated cards and started chart.js integrations

--- client/main.js
+++ client/main.js
@@ -1,12 +1,20 @@
 console.log("Hello to whoever is trying to inspect this page right now! I hope you're having a great day :)!")
 
-
-button = document.getElementById("creatNewBtn")
-console.log(button)
-
-button.addEventListener("click", function(){
-	console.log("IS THIS WORKING")
-	document.getElementById("demo").innerHTML = "Hello World";
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    datasets: [{
+      label: 'apples',
+      data: [12, 19, 3, 17, 6, 3, 7],
+      backgroundColor: "rgba(153,255,51,0.4)"
+    }, {
+      label: 'oranges',
+      data: [2, 29, 5, 5, 2, 3, 10],
+      backgroundColor: "rgba(255,153,0,0.4)"
+    }]
+  }
 });
 
 
